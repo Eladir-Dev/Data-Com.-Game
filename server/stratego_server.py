795f8756a3e7debf45dd16ef5a811f3f049ccf2d
Eladir-Dev <ejmatosperez@gmail.com>
2025-08-19 18:21:10 +0000

    Se trabajo un poco el codigo, pero todavia no esta funcional

--- server/stratego_server.py
+++ server/stratego_server.py
@@ -1,6 +1,7 @@
 """
 Aque se encuentra la logica de stratego
 """
+import random
 # El cliente (jugador) va a tener listo su deck antes de empezar el juego
 
 # recoger queue 
@@ -9,14 +10,50 @@ Aque se encuentra la logica de stratego
 # recoger comando
 # empezar partida (el rojo empieza)
 
-def create_board(user1: str, deck2: list[list[str]], user2: str, deck2: list[list[str]]):
+# unit librar:
+#   's', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b', 'f'
+
+def create_board(user1: str, deck1: list[list[str]], user2: str, deck2: list[list[str]]):
   board = [["" for _ in range (10)]for _ in range (10)]
+  team = random.uniform(0, 1)
+
+  if  (team<1):
+    deck1 = set_team("R", deck1) # R = team red
+    deck2 = set_team("B", deck2) # B = team blue
+  else:
+    deck1 = set_team("B", deck1) # R = team red
+    deck2 = set_team("R", deck2) # B = team blue
+
+  for row in deck1:
+    for element in row:
+      board[row][element] = deck[row][element]
+
+
+
+def set_team(team: str, deck: list[list[str]]):
+  for row in deck:
+    for element in row:
+      deck[row][element] = deck[row][element] + team
+  return deck
   
 
 #[0R][SR][FR][1R][9R]
 #[0R][SR][FR][1R][9R]
 
-def main():
+def create_debug_deck():
+  deck = [["" for _ in range (10)]for _ in range (4)]
+  print(deck)
+
+def print_board():
   pass
+
+def main():
+  """
+  This main method is used to debug Stratego's logic
+  """
+  print("Hello their")
+  create_debug_deck()
+
+
 if __name__ == "__main__":
   main()
