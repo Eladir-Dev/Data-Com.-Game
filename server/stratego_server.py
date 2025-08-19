00ce622ae8bff57fb7cf87ef8faea30459a55e9b
Eladir-Dev <ejmatosperez@gmail.com>
2025-08-19 11:00:42 -0400

    Update stratego_server.py

--- server/stratego_server.py
+++ server/stratego_server.py
@@ -9,8 +9,14 @@ Aque se encuentra la logica de stratego
 # recoger comando
 # empezar partida (el rojo empieza)
 
-def create_board(user1:str, deck2:list[list[str]], user2:str, deck2:list[list[str]]):
+def create_board(user1: str, deck2: list[list[str]], user2: str, deck2: list[list[str]]):
+  board = [["" for _ in range (10)]for _ in range (10)]
   
 
-[0R][SR][FR][1R][9R]
-[0R][SR][FR][1R][9R]
+#[0R][SR][FR][1R][9R]
+#[0R][SR][FR][1R][9R]
+
+def main():
+  pass
+if __name__ == "__main__":
+  main()
