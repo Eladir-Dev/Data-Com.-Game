40007aa866939528fced3229d1f24e42b09480fd
Eladir-Dev <ejmatosperez@gmail.com>
2025-08-19 10:41:11 -0400

    Create stratego.py

---
+++ client/stratego.py
@@ -0,0 +1,15 @@
+"""
+Aque se encuentra la logica de stratego
+"""
+# El cliente (jugador) va a tener listo su deck antes de empezar el juego
+
+# recoger queue 
+# leer los decks
+# prepara el tablero
+# recoger comando
+# empezar partida (el rojo empieza)
+
+
+
+[0R][SR][FR][1R][9R]
+[0R][SR][FR][1R][9R]
