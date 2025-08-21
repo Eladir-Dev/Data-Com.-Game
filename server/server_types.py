f2786a40d1b9f3be0bbc1a8af680f0b49ca9d30f
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-08-21 18:07:22 -0400

    added documentation to the `server_types` module

--- server/server_types.py
+++ server/server_types.py
@@ -1,6 +1,9 @@
-
 from socket import socket
 
+# Re-export Python's `socket` type under a different name so that it doesn't conflict with
+# the name of the module, which is also named `socket`.
 Connection = socket
 
+# The size of the socket's buffer for receiving and sending messages 
+# between the server and the client.
 BUF_SIZE = 1024
