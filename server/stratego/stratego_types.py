6f37d4102e8f19f97b2c4a3d7a2d416f0ee92f39
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-08-21 18:00:22 -0400

    added documentation to `stratego_types` module

--- server/stratego/stratego_types.py
+++ server/stratego/stratego_types.py
@@ -1,13 +1,15 @@
-import socket
-from socket import socket as Connection
-import threading
-import time
 from typing import Literal
 
-# Types.
+# === Types ===
+
+# The color of a Stratego player or piece.
 StrategoColor = Literal['r', 'b']
 
-# Constants.
+# === Constants ===
+
+# The dimensions of a board.
 ROWS = 10
 COLS = 10
+
+# The amount of rows that a player's starting deck has.
 DECK_ROWS = 4
