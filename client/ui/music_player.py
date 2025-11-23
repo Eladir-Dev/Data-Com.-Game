20a8b4cb54817f6437a7c1bb2a28a0b20ac983f8
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-11-23 18:17:08 -0400

    feat(stratego): added background music

---
+++ client/ui/music_player.py
@@ -0,0 +1,23 @@
+import pygame
+from pathlib import Path
+
+GAMES_DIR = Path(__file__).parent.parent / "games"
+
+STRATEGO_MUSIC_PATH = GAMES_DIR / "stratego" / "sfx" / "Brirfing_theme.mp3"
+SECRET_GAME_MUSIC_PATH = GAMES_DIR / "secret_game" / "assets" / "Bone Yard Waltz - Loopable.ogg"
+
+def play_stratego_bg_music():
+    _play_looping_bg_music(STRATEGO_MUSIC_PATH)
+
+
+def play_secret_game_bg_music():
+    _play_looping_bg_music(SECRET_GAME_MUSIC_PATH)
+
+
+def _play_looping_bg_music(path: Path):
+    pygame.mixer.music.load(path)
+    pygame.mixer.music.play(-1)
+
+
+def stop_all_bg_music():
+    pygame.mixer.music.stop()
