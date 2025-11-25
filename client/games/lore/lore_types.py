c8170b4b3c12192cd9d5aa4233aa548ed9d9b19d
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-11-24 21:12:18 -0400

    lore map now loads from the map files

--- client/games/lore/lore_types.py
+++ client/games/lore/lore_types.py
@@ -1,12 +1,18 @@
 from typing import Literal
+from pathlib import Path
 
 LoreKind = Literal['secret_game', 'secret_dlc_store', 'secret_paint_game']
 
+MAPS_DIR = Path(__file__).parent / "maps"
 
 class LoreMap:
     def __init__(self, lore_kind: LoreKind):
-        # load the map from the file corresponding to the lore kind
-        pass
+        # Load the map from the file corresponding to the lore kind.
+        self.tiles = []
+        file_name = f"{MAPS_DIR}/{lore_kind}.txt"
 
-
+        with open(file_name, "r") as f:
+            for raw_line in f.readlines():
+                line = raw_line.rstrip()
+                self.tiles.append(list(line))
 
