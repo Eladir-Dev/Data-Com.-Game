b3d832299a69b23db9a5f52ff9ac6d688b30f4d1
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-11-28 18:50:55 -0400

    fix(lore): decreased secret game lore unlock chance after finishing a Stratego or Word Golf game

--- client/games/lore/lore_unlocking.py
+++ client/games/lore/lore_unlocking.py
@@ -3,8 +3,7 @@ from common_types.game_types import GameKind
 from games.lore.lore_types import LoreKind
 from common_types.global_state import GlobalClientState, LoreGlobalState
 
-# TODO: These are currently set to 100% for testing purposes; they should be set to a lower percent in practice.
-_SECRET_GAME_CUTSCENE_UNLOCK_CHANCE = 1.0
+_SECRET_GAME_CUTSCENE_UNLOCK_CHANCE = 0.25
 _SECRET_DLC_STORE_CUTSCENE_UNLOCK_CHANCE = 1.0
 
 def _try_unlock_secret_game_lore() -> LoreKind | None:
